5f6e02b1c098ba371f856a8b1c861fdba729851a
Joanna K <joannakleinrm@gmail.com>
2023-02-25 17:42:33 +0100

    lightbox js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const gallery = document.querySelector(".gallery");
+const galleryItemsGrid = galleryItems
+    .map((image) => {
+        return `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`;
+    })
+    .join("");
+
+gallery.innerHTML = galleryItemsGrid;
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    function closeModal(event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
